cb4bdbd7448e7f248c030863d6e2419c33d26869
PanktiPatel99 <panktip.mobio@gmail.com>
2022-08-29 19:01:52 +0530

    implement search functionality

--- src/Component/Search.js
+++ src/Component/Search.js
@@ -1,12 +1,38 @@
+import React, { useState } from "react";
 import "./Search.css";
 import Data from "./Data";
 
-const Search = (props) => {
+const Search = () => {
+  const [filterItems, setFilterItems] = useState("");
+  const [searchItem] = useState(["title"]);
+
+  const searchHandle = (e) => {
+    setFilterItems(e.target.value);
+    // console.log(e.target.value);
+    // console.log(filterItems);
+  };
+
+  let dataSearch = Data.filter((item) => {
+    return searchItem.some((newItem) => {
+      return (
+        item[newItem]
+          .toString()
+          .toLowerCase()
+          // .indexOf(filterItems.toLowerCase()) > -1
+          .includes(filterItems.toString().toLowerCase())
+      );
+    });
+  });
+  // console.log(dataSearch);
+
   return (
     <div>
-      <div className="search">Search Image </div>
+      <div className="search">
+        <label>Search Images: </label>
+        <input onChange={searchHandle} value={filterItems} type="text" />
+      </div>
       <div className="display_images">
-        {Data.map((data, i) => {
+        {dataSearch.map((data, i) => {
           return (
             <>
               <div key={i} className="img_container">
